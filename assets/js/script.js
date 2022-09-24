187ee71885b617e9b0e4df30d74d5ab7519a9e14
Andrea Hergert <ahergert24@hotmail.com>
2022-09-23 21:02:46 -0700

    changed some variables, got form to work, now dealing with other errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,56 +3,19 @@
 // var forecastedUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=current,minutely,hourly,alerts&appid=" + apiKey;
 
 
-// weather.fetchWeather ("Seattle") in console
 // Seattle Geocodes Geo coords [47.6062, -122.3321]
-// Need Dates (dt) converted to something readable, moment?
 // Need 5-day forecast
-// Need search button to event listen for var weather?
 // Need local storage
 
 
 
 var apiKey = "bd55593ecb666c01d38f4ec9276324e8";
 var currentDate = moment().format('l');
-var searchedCities = [];
 var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#city');
 var containerEl = document.querySelector("#current-city")
 
-
-function fetchWeather(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey)
-
-        .then((response) => {
-            if (!response.ok) {
-                alert("No weather found.");
-            }
-            return response.json();
-        })
-        .then((data) => this.displayWeather(data));
-};
-
-
-function displayWeather(data) {
-    var { name } = data;
-    var { icon } = data.weather[0];
-    var { temp } = data.main;
-    var { speed } = data.wind;
-    var { humidity } = data.main;
-    console.log(name, icon, temp, speed, humidity)
-    document.querySelector(".city-shown").innerText = name;
-    document.querySelector(".date").innerText = currentDate;
-    document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
-    document.querySelector(".wind").innerText =
-        "Wind: " + speed + " MPH";
-    document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-};
-
-// Prevent Default
-function handleFormSubmit (event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
     var city = cityInputEl.value.trim();
@@ -67,5 +30,37 @@ function handleFormSubmit (event) {
     }
 };
 
+var fetchWeather = function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey)
+
+        .then((response) => {
+            if (!response.ok) {
+                alert("No weather found.");
+            }
+            return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+};
+
+
+var displayWeather = function (data) {
+    var { name } = data;
+    var { icon } = data.weather[0];
+    var { temp } = data.main;
+    var { speed } = data.wind;
+    var { humidity } = data.main;
+    console.log(name, icon, temp, speed, humidity)
+    document.querySelector(".city-shown").innerText = name;
+    // currentDate pulled from moment
+    document.querySelector(".date").innerText = currentDate;
+    document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
+    document.querySelector(".wind").innerText =
+        "Wind: " + speed + " MPH";
+    document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+};
+
 fetchWeather("Seattle");
-userFormEl.addEventListener('submit', fetchWeather);
+userFormEl.addEventListener('submit', formSubmitHandler);
