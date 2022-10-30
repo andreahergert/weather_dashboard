973abcc9217669627612424a31e932e373bbe472
Andrea Hergert <ahergert24@hotmail.com>
2022-10-30 13:22:55 -0700

    updated five day forecast and saved cities feature changed some other functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,115 +1,124 @@
-// var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-
-// var forecastedUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=current,minutely,hourly,alerts&appid=" + apiKey;
-
-// var futureDate = moment().add(i + 1, "days").format("MM/DD/YYYY");
-
-// Seattle Geocodes Geo coords [47.6062, -122.3321]
-// Need 5-day forecast
-// Need local storage
-
-
 var apiKey = "bd55593ecb666c01d38f4ec9276324e8";
-var currentDate = moment().format('l');
-var userFormEl = document.querySelector('#user-form');
-var cityInputEl = document.querySelector('#city');
-var containerEl = document.querySelector("#current-city");
-var btnInsert = document.querySelector('#fetch-button');
-var lsOutput = document.querySelector('#past-search-buttons')
-var lat = "47.6062";
-var lon = "-122.3321";
 
-// btnInsert.onclick = function () {
-//     for (var i = 0; i < localStorage.length; i++)
-//     if (localStorage.getItem("city") !== null) {
-//         cityInputEl = JSON.parse(localStorage.getItem("city"));
-//     }
-//     lsOutput.innerHTML(`${city} ${value}`);
-//     localStorage.setItem("city", JSON.stringify(cityInputEl));
-// }
+var city = "";
+var searchCity = $("#city-input");
+var searchButton = $("#search-button");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidity = $("#humidity");
+var currentWind = $("#wind-speed");
+var sCity = [];
 
-
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    var city = cityInputEl.value.trim();
-    if (city) {
-        fetchWeather(city);
-        cityInputEl.value = '';
-        lsOutput.innerHTML = city;
-    } else {
-        alert('Please enter a City');
+function find(c) {
+    for (var i = 0; i < sCity.length; i++) {
+        if (c.toUpperCase() === sCity[i]) {
+            return -1;
+        }
     }
-};
+    return 1;
+}
 
-// Fetches weather by city (default is Seattle) from third party api
-var fetchWeather = function (city) {
-    // inserted &units=imperial into link so that this will display fahrenheit 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey)
-        .then((response) => {
-            if (!response.ok) {
-                alert("No weather found.");
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city = searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+// Current forecast
+function currentWeather(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+    $.ajax({
+        url: apiUrl,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        var weathericon = response.weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+        var date = new Date(response.dt * 1000).toLocaleDateString();
+        $(currentCity).html(response.name + " " + "(" + date + ")" + "<img src=" + iconurl + ">");
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        $(currentTemperature).html(" " + (tempF).toFixed(2) +" " +  "&#8457");
+        $(currentHumidity).html(" " + response.main.humidity +" " +  "%");
+        var ws = response.wind.speed;
+        var windsmph = (ws * 2.237).toFixed(1);
+        $(currentWind).html(" " + windsmph +" " +  "MPH");
+
+        forecast(response.id);
+        if (response.cod == 200) {
+            sCity = JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if (sCity == null) {
+                sCity = [];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname", JSON.stringify(sCity));
+                addToList(city);
             }
-            return response.json();
-        })
-        .then((data) => this.displayWeather(data));
-};
-
-// var displayWeather is made by using a function to gather what data we want from the third party api.  
-var displayWeather = function (data) {
-    var { name } = data;
-    var { icon } = data.weather[0];
-    var { temp } = data.main;
-    var { speed } = data.wind;
-    var { humidity } = data.main;
-    console.log(name, icon, temp, speed, humidity)
-    document.querySelector(".city-shown").innerText = name;
-    // currentDate pulled from moment due to strange date format from openweather api
-    document.querySelector(".date").innerText = currentDate;
-    // icon made by copying the icon url and inserting the specific icon code into the icon field
-    document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
-    document.querySelector(".wind").innerText =
-        "Wind: " + speed + " MPH";
-    document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-
-};
-
-var fetchWeatherFiveDay = function (lat, lon) {
-    // inserted &units=imperial into link so that this will display fahrenheit 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=current,minutely,hourly,alerts&appid=" + apiKey)
-        .then((response) => {
-            if (!response.ok) {
-                alert("No weather found.");
+            else {
+                if (find(city) > 0) {
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname", JSON.stringify(sCity));
+                    addToList(city);
+                }
             }
-            return response.json();
-        })
-        .then((data) => this.displayWeatherFiveDay(data));
+        }
+    });
+}
 
-};
+// Five Day forecast
+function forecast(cityid) {
+    var fiveUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + apiKey;
+    $.ajax({
+        url: fiveUrl,
+        method: "GET"
+    }).then(function (response) {
+        for (i = 0; i < 5; i++) {
+            var date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+            var iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+            var tempK = response.list[((i + 1) * 8) - 1].main.temp;
+            var tempF = (((tempK - 273.5) * 1.80) + 32).toFixed(2);
+            var humidity = response.list[((i + 1) * 8) - 1].main.humidity;
 
-// var displayWeatherFiveDay = function (data) {
-//     var { icon } = data.list.weather[0];
-//     var { temp } = data.list.main;
-//     var { speed } = data.list.wind;
-//     var { humidity } = data.list.main;
-//     console.log(icon, temp, speed, humidity)
-//     // currentDate pulled from moment due to strange date format from openweather api
-//     // document.querySelector(".date1").innerText = currentDate;
-//     // icon made by copying the icon url and inserting the specific icon code into the icon field
-//     document.querySelector(".icon1").src =
-//         "https://openweathermap.org/img/wn/" + icon + ".png";
-//     document.querySelector(".temp1").innerText = "Temp: " + temp + "°F";
-//     document.querySelector(".wind1").innerText =
-//         "Wind: " + speed + " MPH";
-//     document.querySelector(".humidity1").innerText =
-//         "Humidity: " + humidity + "%";
-// };
+            $("#Date" + i).html(date);
+            $("#Img" + i).html("<img src=" + iconurl + ">");
+            $("#Temp" + i).html(" " + tempF + " &#8457");
+            $("#Humidity" + i).html(" " + humidity + " %");
+        }
+    });
+}
 
+function addToList(c) {
+    var listEl = $("<li>" + c.toUpperCase() + "</li>");
+    $(listEl).attr("class", "list-group-item");
+    $(listEl).attr("data-value", c.toUpperCase());
+    $(".list-group").append(listEl);
+}
 
+function invokePastSearch(event) {
+    var liEl = event.target;
+    if (event.target.matches("li")) {
+        city = liEl.textContent.trim();
+        currentWeather(city);
+    }
+}
 
-// Default city Seattle on page load
-fetchWeather("Seattle");
-// fetchWeatherFiveDay("47.6062", "-122.3321");
-userFormEl.addEventListener('submit', formSubmitHandler);
+function loadlastCity() {
+    $("ul").empty();
+    var sCity = JSON.parse(localStorage.getItem("cityname"));
+    if (sCity !== null) {
+        sCity = JSON.parse(localStorage.getItem("cityname"));
+        for (i = 0; i < sCity.length; i++) {
+            addToList(sCity[i]);
+        }
+        city = sCity[i - 1];
+        currentWeather(city);
+    }
+}
+
+$("#search-button").on("click", displayWeather);
+$(document).on("click", invokePastSearch);
+$(window).on("load", loadlastCity);
